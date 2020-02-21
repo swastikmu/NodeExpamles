83263b90d3602265d9bde60f71a66c09a741336a
swastikmu <sanu.mukherjee@gmail.com>
2020-02-21 17:35:55 +0530

    project vidly genere rest api post method added

--- projVidly/index.js
+++ projVidly/index.js
@@ -3,29 +3,46 @@ const Joi = require('joi');
 
 app = express();
 
-app.use(express.json());
+app.use(express.json());//parse the json body request
+//app.use(express.urlencoded()); //if the body send in sey=value&key=value type in the request url
 
-var genere = [ 'horror' ,'Action' , 'comedy' , 'romantic' , 'cult' , 'rom-com' , 'thriller' , 'neo-noir'];
+
+var generes = [ 
+{id: 1 , genere : "Action"} ,
+{id: 2 , genere : "Neo-noir"} ,
+{id: 3 , genere : "Horror"} ,
+{id: 4 , genere : "Comedy"} ,
+{id: 5 , genere : "Romantic"} ,
+{id: 6 , genere : "Cult"} ,
+{id: 7 , genere : "Rom-com"} ,
+{id: 8 , genere : "Thriller"} 
+
+];
 
 app.get('/api/genere', (req, res)=>
 {
-res.send(genere);
+res.send(generes);
 }
 );
 
 app.post('/api/genere', (req, res) =>
 {
 
-const find = genere.find( c => c === req.params);
+const find = generes.find( c => c.genere == req.body.genere);
 if (find) {
 res.status(400).send("Already exsist");
+return;
 }
-
+console.log(req.params.genere);
+const oGenere = {
+    id: generes.length + 1,
+    genere: req.body.genere
+}
 
 
 const schema = {
 genere: Joi.string().required()
-}
+};
 
 const result = Joi.validate(req.body, schema);
 
@@ -33,12 +50,11 @@ if (result.error) {
 res.status(400).send(result.error.details[0].message);
 return;
 };
-genere.push(find);
-
-
+generes.push(oGenere);
+res.status(201).send(oGenere);
 }
-
 )
+
 
 
 
